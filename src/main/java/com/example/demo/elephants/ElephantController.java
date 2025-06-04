bf0ca952ebf55fd05bf3b0e0b736300709652bca
Rishav Patel <rishav.s.patel@gmai.com>
2025-06-04 00:24:25 -0400

    3rd

---
+++ src/main/java/com/example/demo/elephants/ElephantController.java
@@ -0,0 +1,80 @@
+package com.example.demo.elephants;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ElephantController {
+
+  @Autowired
+  private ElephantService elephantService;
+
+  @GetMapping("/elephants")
+  public Object getAllElephants() {
+    return elephantService.getAllElephants();
+  }
+
+  @GetMapping("/elephants/{id}")
+  public Elephant getElephantsById(@PathVariable long id) {
+    return elephantService.getElephantsById(id);
+  }
+
+  @GetMapping("/elephants/name")
+  public Object getElephantsByName(@RequestParam String key) {
+    if (key != null) {
+      return elephantService.getElephantsByName(key);
+    } else {
+      return elephantService.getAllElephants();
+    }
+
+  }
+
+  @GetMapping("/elephants/gender/{gender}")
+  public Object getElephantsByGender(@PathVariable String gender) {
+    return elephantService.getElephantsByGender(gender);
+  }
+
+  @GetMapping("/elephants/adults")
+  public Object getAdultElephants() {
+    return new ResponseEntity<>(elephantService.getAdultElephants(), HttpStatus.OK);
+
+  }
+
+  @PostMapping("/elephants")
+  public Object addElephant(@RequestBody Elephant elephant) {
+    return elephantService.addElephant(elephant);
+  }
+
+  @PutMapping("/elephants/{id}")
+  public Elephant updateElephant(@PathVariable Long id, @RequestBody Elephant elephant) {
+    elephantService.updateElephant(id, elephant);
+    return elephantService.getElephantsById(id);
+  }
+
+  @DeleteMapping("/elephants/{id}")
+  public Object deleteElephant(@PathVariable Long id) {
+    elephantService.deleteElephant(id);
+    return elephantService.getAllElephants();
+  }
+
+  @PostMapping("/elephants/writeFile")
+  public Object writeJson(@RequestBody Elephant elephant) {
+    return elephantService.writeJson(elephant);
+  }
+
+  @GetMapping("/elephants/readFile")
+  public Object readJson() {
+    return elephantService.readJson();
+
+  }
+
+}
---
+++ src/main/java/com/example/demo/elephants/ElephantService.java
@@ -0,0 +1,73 @@
+package com.example.demo.elephants;
+
+import java.io.IOException;
+import java.io.File;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Service
+public class ElephantService {
+
+  @Autowired
+  private ElephantRepository elephantRepository;
+
+  public Object getAllElephants() {
+    return elephantRepository.findAll();
+  }
+
+  public Elephant getElephantsById(@PathVariable long studentId) {
+    return elephantRepository.findById(studentId).orElse(null);
+  }
+
+  public Object getElephantsByName(String name) {
+    return elephantRepository.getElephantsByName(name);
+  }
+
+  public Object getElephantsByGender(String gender) {
+    return elephantRepository.findByGender(gender);
+  }
+
+  public Object getAdultElephants() {
+    return elephantRepository.findAdultElephants();
+  }
+
+  public Elephant addElephant(Elephant elephant) {
+    return elephantRepository.save(elephant);
+  }
+
+  public Elephant updateElephant(Long elephantId, Elephant elephant) {
+    return elephantRepository.save(elephant);
+  }
+
+  public void deleteElephant(Long elephantId) {
+    elephantRepository.deleteById(elephantId);
+  }
+
+  public String writeJson(Elephant elephant) {
+    ObjectMapper objectMapper = new ObjectMapper();
+    try {
+      objectMapper.writeValue(new File("elephants.json"), elephant);
+      return "Elephant written to JSON file successfully";
+    } catch (IOException e) {
+      e.printStackTrace();
+      return "Error writing student to JSON file";
+    }
+
+  }
+
+  public Object readJson() {
+    ObjectMapper objectMapper = new ObjectMapper();
+    try {
+      return objectMapper.readValue(new File("elephants.json"), Elephant.class);
+    } catch (IOException e) {
+      e.printStackTrace();
+      return null;
+    }
+
+  }
+
+}
