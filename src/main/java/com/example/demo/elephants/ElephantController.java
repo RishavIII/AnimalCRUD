1877f1d67b2572941998f2dba28471716b3729d4
Rishav Patel <rishav.s.patel@gmai.com>
2025-06-11 14:43:49 -0400

    nothing

--- src/main/java/com/example/demo/elephants/ElephantController.java
+++ src/main/java/com/example/demo/elephants/ElephantController.java
@@ -1,16 +1,14 @@
 package com.example.demo.elephants;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+
 @Controller
 public class ElephantController {
 
@@ -18,62 +16,90 @@ public class ElephantController {
   private ElephantService elephantService;
 
   @GetMapping("/elephants")
-  public Object getAllElephants() {
-    return elephantService.getAllElephants();
+  public String getAllElephants(Model model) {
+    model.addAttribute("elephants", elephantService.getAllElephants());
+    model.addAttribute("title", "All Elephants");
+    return "index";
   }
 
   @GetMapping("/elephants/{id}")
-  public Elephant getElephantsById(@PathVariable long id) {
-    return elephantService.getElephantsById(id);
+  public String getElephantsById(@PathVariable long id, Model model) {
+    model.addAttribute("elephant", elephantService.getElephantsById(id));
+    model.addAttribute("title", "Elephant #: " + id);
+    return "details";
   }
 
   @GetMapping("/elephants/name")
-  public Object getElephantsByName(@RequestParam String key) {
+  public String getElephantsByName(@RequestParam String key, Model model) {
     if (key != null) {
-      return elephantService.getElephantsByName(key);
+      model.addAttribute("elephants", elephantService.getElephantsByName(key));
+      model.addAttribute("title", "Elephants By Name: " + key);
+      return "index";
     } else {
-      return elephantService.getAllElephants();
+      return "redirect:/elephants/";
     }
-
   }
 
   @GetMapping("/elephants/gender/{gender}")
-  public Object getElephantsByGender(@PathVariable String gender) {
-    return elephantService.getElephantsByGender(gender);
+  public String getElephantsByGender(@PathVariable String gender, Model model) {
+    model.addAttribute("elephants", elephantService.getElephantsByGender(gender));
+    model.addAttribute("title", "Elephants By Gender: " + gender);
+    return "index";
   }
 
   @GetMapping("/elephants/adults")
-  public Object getAdultElephants() {
-    return new ResponseEntity<>(elephantService.getAdultElephants(), HttpStatus.OK);
+  public String getAdultElephants(Model model) {
+    model.addAttribute("elephants", elephantService.getAdultElephants());
+    model.addAttribute("title", "Adult Elephants");
+    return "index";
+  }
 
+  @GetMapping("/elephants/delete/{id}")
+  public String deleteElephant(@PathVariable Long id) {
+    elephantService.deleteElephant(id);
+    return "redirect:/elephants";
+  }
+
+  @GetMapping("/elephants/createForm")
+  public String showCreateForm(Model model) {
+      Elephant elephant = new Elephant();
+      model.addAttribute("elephant", elephant);
+      model.addAttribute("title", "Create New Elephant");
+      return "create";
   }
 
   @PostMapping("/elephants")
-  public Object addElephant(@RequestBody Elephant elephant) {
-    return elephantService.addElephant(elephant);
+  public String addElephant(Elephant elephant) {
+    Elephant createdElephant = elephantService.addElephant(elephant);
+    return "redirect:/elephants/" + createdElephant.getElephantId();
   }
 
-  @PutMapping("/elephants/{id}")
-  public Elephant updateElephant(@PathVariable Long id, @RequestBody Elephant elephant) {
+  @GetMapping("/elephants/updateForm/{id}")
+  public String showUpdateForm(@PathVariable Long id, Model model) {
+      Elephant elephant = elephantService.getElephantsById(id);
+      model.addAttribute("elephant", elephant);
+      model.addAttribute("title", "Update Elephant: " + id);
+      return "update";
+  }
+
+  @PostMapping("/elephants/{id}")
+  public String updateElephant(@PathVariable Long id, Elephant elephant) {
     elephantService.updateElephant(id, elephant);
-    return elephantService.getElephantsById(id);
-  }
-
-  @DeleteMapping("/elephants/{id}")
-  public Object deleteElephant(@PathVariable Long id) {
-    elephantService.deleteElephant(id);
-    return elephantService.getAllElephants();
+    return "redirect:/elephants/" + id;
   }
 
   @PostMapping("/elephants/writeFile")
-  public Object writeJson(@RequestBody Elephant elephant) {
+  public String writeJson(@RequestBody Elephant elephant) {
     return elephantService.writeJson(elephant);
   }
 
   @GetMapping("/elephants/readFile")
   public Object readJson() {
     return elephantService.readJson();
-
   }
 
+  @GetMapping("/elephants/about")
+  public String about() {
+    return "about";
+  }
 }
